939510431f9a9ed7d8d2f2362c5aced9309b02be
patilhharshad <140788444+patilhharshad@users.noreply.github.com>
2024-08-01 15:43:39 +0530

    Add files via upload

--- vision.py
+++ vision.py
@@ -18,7 +18,7 @@ def load_model():
     #     bnb_4bit_use_double_quant=True,
     #     bnb_4bit_quant_type="nf4",
     #     bnb_4bit_compute_dtype=torch.bfloat16
-    )
+    # )
 
     # Load tokenizer
     tokenizer = AutoTokenizer.from_pretrained(model_id)
