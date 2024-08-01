8fcce1e7256e15f5451e54d9871bc4feba42091a
patilhharshad <140788444+patilhharshad@users.noreply.github.com>
2024-08-01 16:20:21 +0530

    Add files via upload

--- vision.py
+++ vision.py
@@ -59,8 +59,8 @@ def preprocess_image(image):
     # This might include resizing, normalization, etc.
     # Return a tensor of the preprocessed image
     preprocess = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
+        transforms.Resize(1024),  # Resize to 1024x1024
+        transforms.CenterCrop(1024),  # Crop to 1024x1024
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
